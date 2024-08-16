48fb9047574e85bdcb8c03868a475651d1aef340
Rômulo Penido <romulo.penido@gmail.com>
2024-08-16 11:18:45 -0300

    fix: always run with user

--- imalive.sh
+++ imalive.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
+
+if [ "$(id -u)" -eq 0 ]; then
+    exec sudo -H -u rpenido $0 "$@"
+fi
+
 project_path="/home/rpenido/Projects/personal/imalive"
 git -C $project_path commit -m "chore: i'm alive" --allow-empty && git -C $project_path push
